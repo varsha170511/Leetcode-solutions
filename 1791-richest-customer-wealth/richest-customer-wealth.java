2b7c894daa9c3d6ca01b24558d466a59d521a460
varsha170511 <varshacb1117@gmail.com>
2025-12-21 14:38:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (10.28%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxwealth=0;
+        for(int i=0;i<accounts.length;i++){
+            int currentwealth=0;
+            for(int j=0;j<accounts[i].length;j++){
+                currentwealth+=accounts[i][j];
+            }
+            maxwealth=Math.max(maxwealth,currentwealth);
+        }
+        return maxwealth;
+        
+    }
+}
